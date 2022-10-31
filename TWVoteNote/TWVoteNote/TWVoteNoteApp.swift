7be30b8b56c83a96f741ead8b5bba5e80fc809fa
allenlinli <allenlinli@gmail.com>
2022-10-31 10:57:28 +0800

    feat: init the project

---
+++ TWVoteNote/TWVoteNote/TWVoteNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWVoteNoteApp.swift
+//  TWVoteNote
+//
+//  Created by Lin Li on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct TWVoteNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
